3908198b410208737e30eae9f76aa2ad4c4d0962
Ihor Mykhalchenko <Ihor Mykhalchenko>
2015-11-29 18:02:00 +0200

    file upload

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -86,10 +86,29 @@ angular.module('app.controllers', [])
 })
 
 
-.controller('regCtrl', function($scope, $state, $ionicPopup, ProfileService) {
+.controller('regCtrl', function($scope, $state, $ionicPopup, ProfileService, Upload, $timeout) {
   $scope.data = {
 
   };
+
+  $scope.uploadPic = function(file) {
+    file.upload = Upload.upload({
+      url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+      data: {file: file}
+    });
+    file.upload.then(function (response) {
+      $timeout(function () {
+        file.result = response.data;
+      });
+    }, function (response) {
+      if (response.status > 0){
+        $scope.errorMsg = response.status + ': ' + response.data;
+      }
+    }, function (evt) {
+      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+    });
+  };
+
   $scope.save = function() {
     ProfileService.save($scope.data).success(function(data) {
       $state.go('app.profile.main');
