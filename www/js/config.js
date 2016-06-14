e55cdbbbcc28ab29c5ee96b38e426c3fd41bc163
ihormihal <ihor.mihal@gmail.com>
2016-06-14 17:14:42 +0300

    toggle swipe

--- www/js/config.js
+++ www/js/config.js
@@ -58,7 +58,7 @@ angular.module('app.config', ['ngResource'])
 
 	/* App & API configuration */
 	$rootScope.config = {
-		version: '0.7.14',
+		version: '0.7.16',
 		domain: 'https://taxfree4u.eu/', //default
 		domains: {
 			public_https: 'https://taxfree4u.eu/',
